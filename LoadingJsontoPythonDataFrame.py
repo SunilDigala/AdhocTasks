7111037afecb60d771db7afca2e57926566aca07
Sunil Digala <sunildigala@outlook.com>
2022-07-29 20:30:34 +0530

    Files Updated

--- LoadingJsontoPythonDataFrame.py
+++ LoadingJsontoPythonDataFrame.py
@@ -1,1 +1,21 @@
-tis a test file to commit in git
+# Python program to read
+# json file
+
+
+import json
+
+# Opening JSON file
+f = open('SourceFiles/Expenses.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+type(data)
+print(data[1])
+# Iterating through the json
+# list
+for i in data:
+	print(i["WHO"])
+# Closing file
+f.close()
